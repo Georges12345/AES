f68e63061c4f57849a979362ace5c74228f35f3c
Georges <99199584+Georges12345@users.noreply.github.com>
2023-04-30 07:49:27 -0400

    Update AES.py

--- AES.py
+++ AES.py
@@ -1,5 +1,51 @@
 import base64
 import secrets
+import os
+from colorama import init, Fore, Style
+
+
+
+def clear_screen():
+      os.system('clear')
+      
+      
+init() #to call colorama
+
+
+def enc_print():
+    print(r'''
+     ____   _    _   ___   ____  __   __  ____   _______
+    / ___| | \  | | / __\ |  _ \ \ \_/ / | __ \ |__   __|
+    | |_   |  \ | |/ /    |  __/  \   /  | ___/    | |
+    |  _|  | |\\| || |    |  \     | |   | |       | |
+    | |__  | | \  |\ \__  | | \    | |   | |       | |
+    \____| |_|  \_| \___/ |_|\_\   |_|   |_|       |_|
+    
+    ''')
+
+def dec_print():
+    print(r'''
+     ___    ____   ___   ____  __   __  ____   _______
+    |   \  / ___| / __\ |  _ \ \ \_/ / | __ \ |__   __|
+    | || | | |_  | /    |  __/  \   /  | ___/    | |
+    | || | |  _| | |    |  \     | |   | |       | |
+    | || | | |__ | \__  | | \    | |   | |       | |
+    |___/  \____| \___/ |_|\_\   |_|   |_|       |_|
+    
+    ''')
+    
+def AES_print():
+    print(r'''
+     _____   ____    ____
+    /  _  \ / ___|  / ___\
+    | |_| | | |_   | |___  
+    |  _  | |  _|   \___ \
+    | | | | | |__   ____| |          
+    |_| |_| \____| |_____/ 
+        
+    ''')    
+
+
 
 def encrypt(plain_text, key):
     cipher_text = bytearray(plain_text)
@@ -12,22 +58,39 @@ def decrypt(cipher_text, key):
     for i in range(len(plain_text)):
         plain_text[i] ^= key[i % len(key)]
     return plain_text
+    
+clear_screen()
+AES_print()	
+
 
 while True:
-    choice = input("Enter 0 for encryption, 1 for decryption, or q to quit: ")
+    print(Fore.YELLOW,"Enter 0 for encryption, 1 for decryption, or q to quit:\n",Fore.RESET)
+    choice = input("[+]")
     if choice == 'q':
         break
     elif choice == '0':
-        plain_text = input("Enter the plain text: ")
+        clear_screen()
+        enc_print()      
+        print(Fore.BLUE,"\nEnter the plain text:\n",Fore.RESET)
+        plain_text = input("[+]")
         key = secrets.token_bytes(16)
-        cipher_text = encrypt(plain_text.encode('latin-1'), key)
-        print("Cipher text: " + cipher_text.decode('latin-1'))
-        print("Key: " + base64.urlsafe_b64encode(key).decode('latin-1'))
+        cipher_text = encrypt(plain_text.encode('latin-1'), key)  
+        print("\nCipher text: ",end="")
+        print(Fore.GREEN,cipher_text.decode('latin-1'),Fore.RESET)
+        print("Key: ",end="")
+        print(Fore.GREEN,base64.urlsafe_b64encode(key).decode('latin-1')+"\n",Fore.RESET)
+        
+            
+            
     elif choice == '1':
-        cipher_text = input("Enter the ciphertext text: ")
-        key = input("Enter the encryption key: ")
+        clear_screen()
+        dec_print()
+        print(Fore.BLUE,"\nEnter the ciphertext:\n",Fore.RESET)
+        cipher_text = input("[+]")
+        print(Fore.BLUE,"\nEnter the encryption key:\n",Fore.RESET)
+        key = input("[+]")
         key_bytes = base64.urlsafe_b64decode(key.encode('latin-1') + b'=' * (4 - len(key) % 4))
         decrypted_text = decrypt(cipher_text.encode('latin-1'), key_bytes)
-        print("Decrypted text: " + decrypted_text.decode('latin-1'))
+        print("\nDecrypted text: " + decrypted_text.decode('latin-1')+"\n")
     else:
         print("Invalid choice.")
